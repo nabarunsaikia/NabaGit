ccbd20a6dee1424010c0d4d678b29c698dd84695
nabarun-pc <nabarunsaikia36@gmail.com>
2016-12-13 22:16:49 +0530

    Commit1

---
+++ GitTest/src/com/naba/app/App.java
@@ -0,0 +1,13 @@
+package com.naba.app;
+
+import com.naba.controller.Controller;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git2");
+		new Controller().print();
+	}
+
+}
---
+++ GitTest/src/com/naba/controller/Controller.java
@@ -0,0 +1,7 @@
+package com.naba.controller;
+
+public class Controller {
+	public void print(){
+		System.out.println("print");
+	}
+}
